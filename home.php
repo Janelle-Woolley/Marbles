51934649fd5674cbd86df586be2e73b77d7bf5f0
Janelle-Woolley <103225532+Janelle-Woolley@users.noreply.github.com>
2023-09-11 13:54:38 +1200

    Sprint 3 -

--- home.php
+++ home.php
@@ -34,12 +34,24 @@
 			
 			<!-- nav_bar class from style sheet -->
 			<div class="grid-item nav_bar">
-				nav
+				<nav>
+					<!-- Creates links to each page with names -->
+					<a href="home.php"> HOME </a>
+					<a href="teams.php"> TEAMS </a>
+					<a href="events.php"> EVENT </a>
+            	</nav>
 			</div>
 			
 			<!-- content class from style sheet -->
 			<div class="grid-item home_content">
-				home content
+				<p>
+					Jelle's Marble Runs, also known as Jelle's Marble Race, is a popular YouTube channel and online sports phenomenon created by Jelle Bakker and Dion Bakker. This
+					unique and imaginative series brings the world of sports to life using marbles as athletes. The concept revolves around various marble teams competing in intricate
+					and creatively designed courses that mimic real-world sports events like track and field, swimming, and even the Olympics. What started as a hobby has grown into a
+					global sensation, with millions of fans eagerly following the races and supporting their favorite marble teams. Jelle's Marble Runs is not just a source of 
+					entertainment but also a testament to the power of creativity and community, uniting people from around the world in their shared enthusiasm for this quirky and 
+					engaging marble-based sports competition.
+				</p>
 			</div>
 			
 			
