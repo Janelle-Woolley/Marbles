04e7857dc62f3195e5a5f3f470980d1139b1ad79
Janelle-Woolley <103225532+Janelle-Woolley@users.noreply.github.com>
2023-09-24 14:46:36 +1300

    Sprint 5 - added header and footer

--- insert_marble.php
+++ insert_marble.php
@@ -13,10 +13,77 @@
     </head>
 
     <body>
+			<!-- opens php -->
+    	<?php
+			session_start();
+			include '../marbles_mysqli.php';
+
+		?>
+		<!-- creates grid -->
+		<div class="grid-container">
+			<!-- logo class from style sheet -->
+			<div class="grid-item logo">
+				 <img src="images/logo.png" alt="Jelle's Marble Run's Logo" width="200" height="105">
+			</div>
+			
+			<!-- banner class from style sheet -->
+			<div class="grid-item banner">
+				Jelle's Marble Race
+			</div>
+			
+			<!-- search_bar class from style sheet -->
+			<div class="grid-item search_bar">
+				<form method="post" action="search.php">
+                	<input type="text" name="search">
+                	<input type="submit" name="submit" value="Search" class="search_button">
+         		</form>
+			</div>
+			
+			<!-- nav_bar class from style sheet -->
+					<div class="grid-item login_bar">
+					<nav>
+					<?php
+					// debug code
+					if((!isset($_SESSION['logged_in'])) or $_SESSION['logged_in'] != 1){
+						echo "<a href='login.php'> LOGIN </a>";
+					}
+					else {
+						echo "Logged In: ".$_SESSION['username'];
+						echo "<a href='process_logout.php'> LOGOUT </a>";
+					}
+					?>
+					</nav>
+					</div>
+					<div class="grid-item nav_bar">
+					<nav>
+					<!-- Creates links to each page with names -->
+					<a href="home.php"> HOME </a>
+					<a href="teams.php"> TEAMS </a>
+					<a href="events.php"> EVENT </a>
+					</nav>
+					</div>
+					<div class="grid-item admin_bar">
+					<nav>
+						<?php
+						if(isset($_SESSION['logged_in'])){
+						$username = $_SESSION['username'];
+						$user_rank_query = "SELECT * FROM users WHERE username = '$username'";
+						$user_rank_result = mysqli_query($conn, $user_rank_query);
+						$user_rank_record = mysqli_fetch_assoc($user_rank_result);
+
+						if($user_rank_record['rank'] == "admin" || $user_rank_record['rank'] == "owner"){
+							echo "<a href='admin.php'> ADMIN </a>";
+						}
+						if($user_rank_record['rank'] == "owner"){
+							echo "<a href='owner.php'> OWNER </a>";
+						}
+						}
+						?>
+					</nav>
+					</div>
+		</div>
 	<!-- opens php -->
 	<?php
-		session_start();
-		include '../marbles_mysqli.php';
 		
 		$competitor_name = $_POST['marble_name'];
 		$team_id = $_POST['marble_team'];
@@ -26,7 +93,7 @@
 		
 		if(!mysqli_query($conn, $insert_competitors)){
 			echo 'Unable to add Marble';
-			header("refresh:5; url = home.php");
+			header("refresh:2; url = admin.php");
 		}
 		
 		$competitor_id_query = "SELECT competitor_id FROM competitors WHERE competitor_name = '$competitor_name'";
@@ -38,12 +105,17 @@
 		
 		if(!mysqli_query($conn, $insert_competitors_roles)){
 			echo 'Unable to add Marble';
-			// add delete competitor because we don't want a competitor without a role
+			// TO ADD: delete competitor because we don't want a competitor without a role
 		} else {
 			echo 'Marble Added';
-		}
-		
-		header("refresh:2; url = home.php");
+			header("refresh:2; url = admin.php");
+		}		
 		
 	?>
+	<div class="footer_grid">
+			<!-- footer class from style sheet -->
+			<div class="footer">
+				footer
+			</div>
+		</div>
 	</body>
