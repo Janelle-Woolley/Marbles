14d9a191736f42fda99491391d49381f3d4d758e
Janelle-Woolley <103225532+Janelle-Woolley@users.noreply.github.com>
2023-09-27 00:39:07 +1300

    Sprint 5 - Added team logos

--- teams.php
+++ teams.php
@@ -119,18 +119,19 @@
 					<div id="bumble-bees" class="team-info">
 					<div class="team-info-column">
 						<?php
-							$bumble_bees_query = "SELECT team_code, team_name, hashtag FROM teams WHERE team_id = 12";
+							$bumble_bees_query = "SELECT team_code, team_name, hashtag, image FROM teams WHERE team_id = 12";
 							$bumble_bees_result = mysqli_query($conn, $bumble_bees_query);
 							$bumble_bees_record = mysqli_fetch_assoc($bumble_bees_result);
 							echo $bumble_bees_record['team_code']." - ".$bumble_bees_record['team_name'];
 							echo "<br>";
 							echo "#".$bumble_bees_record['hashtag'];
+							echo "<img src='images/".$bumble_bees_record['image']."' alt='".$bumble_bees_record['team_name']." Logo' width=100, height=124>";
 						?>
 					</div>
 					<div class="team-info-column">
 						<?php
 
-						$bumble_bees_members_query = "SELECT competitors.competitor_name, roles.role, roles.description 
+						$bumble_bees_members_query = "SELECT competitors.competitor_name, roles.role, roles.description
 													  FROM competitors, roles, teams, competitors_roles 
 													  WHERE competitors.team_id = teams.team_id 
 													  AND competitors.competitor_id = competitors_roles.competitor_id 
@@ -159,12 +160,13 @@
 					<div id="balls-of-chaos" class="team-info">
 						<div class="team-info-column">
 						<?php
-							$balls_of_chaos_query = "SELECT team_code, team_name, hashtag FROM teams WHERE team_id = 14";
+							$balls_of_chaos_query = "SELECT team_code, team_name, hashtag, image FROM teams WHERE team_id = 14";
 							$balls_of_chaos_result = mysqli_query($conn, $balls_of_chaos_query);
 							$balls_of_chaos_record = mysqli_fetch_assoc($balls_of_chaos_result);
 							echo $balls_of_chaos_record['team_code']." - ".$balls_of_chaos_record['team_name'];
 							echo "<br>";
 							echo "#".$balls_of_chaos_record['hashtag'];
+							echo "<img src='images/".$balls_of_chaos_record['image']."' alt='".$balls_of_chaos_record['team_name']." Logo' width=100, height=124>";
 							?>
 						</div>
 						<div class="team-info-column">
@@ -198,12 +200,13 @@
 					<div id="crazy-cats-eyes" class="team-info">
 						<div class="team-info-column">
 						<?php
-							$crazy_cats_eyes_query = "SELECT team_code, team_name, hashtag FROM teams WHERE team_id = 13";
+							$crazy_cats_eyes_query = "SELECT team_code, team_name, hashtag, image FROM teams WHERE team_id = 13";
 							$crazy_cats_eyes_result = mysqli_query($conn, $crazy_cats_eyes_query);
 							$crazy_cats_eyes_record = mysqli_fetch_assoc($crazy_cats_eyes_result);
 							echo $crazy_cats_eyes_record['team_code']." - ".$crazy_cats_eyes_record['team_name'];
 							echo "<br>";
 							echo "#".$crazy_cats_eyes_record['hashtag'];
+							echo "<img src='images/".$crazy_cats_eyes_record['image']."' alt='".$crazy_cats_eyes_record['team_name']." Logo' width=100, height=124>";
 						?>
 						</div>
 						<div class="team-info-column">
@@ -238,12 +241,13 @@
 					<div id="chocolatiers" class="team-info">
 						<div class="team-info-column">
 						<?php
-							$chocolatiers_query = "SELECT team_code, team_name, hashtag FROM teams WHERE team_id = 5";
+							$chocolatiers_query = "SELECT team_code, team_name, hashtag, image FROM teams WHERE team_id = 5";
 							$chocolatiers_result = mysqli_query($conn, $chocolatiers_query);
 							$chocolatiers_record = mysqli_fetch_assoc($chocolatiers_result);
 							echo $chocolatiers_record['team_code']." - ".$chocolatiers_record['team_name'];
 							echo "<br>";
 							echo "#".$chocolatiers_record['hashtag'];
+							echo "<img src='images/".$chocolatiers_record['image']."' alt='".$chocolatiers_record['team_name']." Logo' width=100, height=124>";
 							?>
 						</div>
 						<div class="team-info-column">
@@ -277,12 +281,13 @@
 					<div id="team-galactic" class="team-info">
 						<div class="team-info-column">
 						<?php
-							$team_galactic_query = "SELECT team_code, team_name, hashtag FROM teams WHERE team_id = 15";
+							$team_galactic_query = "SELECT team_code, team_name, hashtag, image FROM teams WHERE team_id = 15";
 							$team_galactic_result = mysqli_query($conn, $team_galactic_query);
 							$team_galactic_record = mysqli_fetch_assoc($team_galactic_result);
 							echo $team_galactic_record['team_code']." - ".$team_galactic_record['team_name'];
 							echo "<br>";
 							echo "#".$team_galactic_record['hashtag'];
+							echo "<img src='images/".$team_galactic_record['image']."' alt='".$team_galactic_record['team_name']." Logo' width=100, height=124>";
 						?>
 						</div>
 						<div class="team-info-column">
@@ -316,12 +321,13 @@
 					<div id="green-ducks" class="team-info">
 					<div class="team-info-column">
 						<?php
-							$green_ducks_query = "SELECT team_code, team_name, hashtag FROM teams WHERE team_id = 1";
+							$green_ducks_query = "SELECT team_code, team_name, hashtag, image FROM teams WHERE team_id = 1";
 							$green_ducks_result = mysqli_query($conn, $green_ducks_query);
 							$green_ducks_record = mysqli_fetch_assoc($green_ducks_result);
 							echo $green_ducks_record['team_code']." - ".$green_ducks_record['team_name'];
 							echo "<br>";
 							echo "#".$green_ducks_record['hashtag'];
+							echo "<img src='images/".$green_ducks_record['image']."' alt='".$green_ducks_record['team_name']." Logo' width=100, height=124>";
 						?>
 						</div>
 						<div class="team-info-column">
@@ -355,12 +361,13 @@
 					<div id="gliding-glaciers" class="team-info">
 					<div class="team-info-column">
 						<?php
-							$gliding_glaciers_query = "SELECT team_code, team_name, hashtag FROM teams WHERE team_id = 4";
+							$gliding_glaciers_query = "SELECT team_code, team_name, hashtag, image FROM teams WHERE team_id = 4";
 							$gliding_glaciers_result = mysqli_query($conn, $gliding_glaciers_query);
 							$gliding_glaciers_record = mysqli_fetch_assoc($gliding_glaciers_result);
 							echo $gliding_glaciers_record['team_code']." - ".$gliding_glaciers_record['team_name'];
 							echo "<br>";
 							echo "#".$gliding_glaciers_record['hashtag'];
+							echo "<img src='images/".$gliding_glaciers_record['image']."' alt='".$gliding_glaciers_record['team_name']." Logo' width=100, height=124>";
 						?>
 						</div>
 						<div class="team-info-column">
@@ -394,12 +401,13 @@
 					<div id="midnight-wisps" class="team-info">
 					<div class="team-info-column">
 						<?php
-							$midnight_wisps_query = "SELECT team_code, team_name, hashtag FROM teams WHERE team_id = 7";
+							$midnight_wisps_query = "SELECT team_code, team_name, hashtag, image FROM teams WHERE team_id = 7";
 							$midnight_wisps_result = mysqli_query($conn, $midnight_wisps_query);
 							$midnight_wisps_record = mysqli_fetch_assoc($midnight_wisps_result);
 							echo $midnight_wisps_record['team_code']." - ".$midnight_wisps_record['team_name'];
 							echo "<br>";
 							echo "#".$midnight_wisps_record['hashtag'];
+							echo "<img src='images/".$midnight_wisps_record['image']."' alt='".$midnight_wisps_record['team_name']." Logo' width=100, height=124>";
 						?>
 						</div>
 						<div class="team-info-column">
@@ -433,12 +441,13 @@
 					<div id="minty-maniacs" class="team-info">
 					<div class="team-info-column">
 						<?php
-							$minty_maniacs_query = "SELECT team_code, team_name, hashtag FROM teams WHERE team_id = 16";
+							$minty_maniacs_query = "SELECT team_code, team_name, hashtag, image FROM teams WHERE team_id = 16";
 							$minty_maniacs_result = mysqli_query($conn, $minty_maniacs_query);
 							$minty_maniacs_record = mysqli_fetch_assoc($minty_maniacs_result);
 							echo $minty_maniacs_record['team_code']." - ".$minty_maniacs_record['team_name'];
 							echo "<br>";
 							echo "#".$minty_maniacs_record['hashtag'];
+							echo "<img src='images/".$minty_maniacs_record['image']."' alt='".$minty_maniacs_record['team_name']." Logo' width=100, height=124>";
 						?>
 						</div>
 						<div class="team-info-column">
@@ -472,12 +481,13 @@
 					<div id="mellow-yellow" class="team-info">
 					<div class="team-info-column">
 						<?php
-							$mellow_yellow_query = "SELECT team_code, team_name, hashtag FROM teams WHERE team_id = 2";
+							$mellow_yellow_query = "SELECT team_code, team_name, hashtag, image FROM teams WHERE team_id = 2";
 							$mellow_yellow_result = mysqli_query($conn, $mellow_yellow_query);
 							$mellow_yellow_record = mysqli_fetch_assoc($mellow_yellow_result);
 							echo $mellow_yellow_record['team_code']." - ".$mellow_yellow_record['team_name'];
 							echo "<br>";
 							echo "#".$mellow_yellow_record['hashtag'];
+							echo "<img src='images/".$mellow_yellow_record['image']."' alt='".$mellow_yellow_record['team_name']." Logo' width=100, height=124>";
 						?>
 						</div>
 						<div class="team-info-column">
@@ -511,12 +521,13 @@
 					<div id="orangers" class="team-info">
 					<div class="team-info-column">
 						<?php
-							$orangers_query = "SELECT team_code, team_name, hashtag FROM teams WHERE team_id = 10";
+							$orangers_query = "SELECT team_code, team_name, hashtag, image FROM teams WHERE team_id = 10";
 							$orangers_result = mysqli_query($conn, $orangers_query);
 							$orangers_record = mysqli_fetch_assoc($orangers_result);
 							echo $orangers_record['team_code']." - ".$orangers_record['team_name'];
 							echo "<br>";
 							echo "#".$orangers_record['hashtag'];
+							echo "<img src='images/".$orangers_record['image']."' alt='".$orangers_record['team_name']." Logo' width=100, height=124>";
 						?>
 						</div>
 						<div class="team-info-column">
@@ -550,12 +561,13 @@
 					<div id="pinkies" class="team-info">
 					<div class="team-info-column">
 						<?php
-							$pinkies_query = "SELECT team_code, team_name, hashtag FROM teams WHERE team_id = 8";
+							$pinkies_query = "SELECT team_code, team_name, hashtag, image FROM teams WHERE team_id = 8";
 							$pinkies_result = mysqli_query($conn, $pinkies_query);
 							$pinkies_record = mysqli_fetch_assoc($pinkies_result);
 							echo $pinkies_record['team_code']." - ".$pinkies_record['team_name'];
 							echo "<br>";
 							echo "#".$pinkies_record['hashtag'];
+							echo "<img src='images/".$pinkies_record['image']."' alt='".$pinkies_record['team_name']." Logo' width=100, height=124>";
 						?>
 						</div>
 						<div class="team-info-column">
@@ -589,12 +601,13 @@
 					<div id="team-primary" class="team-info">
 					<div class="team-info-column">
 						<?php
-							$team_primary_query = "SELECT team_code, team_name, hashtag FROM teams WHERE team_id = 6";
+							$team_primary_query = "SELECT team_code, team_name, hashtag, image FROM teams WHERE team_id = 6";
 							$team_primary_result = mysqli_query($conn, $team_primary_query);
 							$team_primary_record = mysqli_fetch_assoc($team_primary_result);
 							echo $team_primary_record['team_code']." - ".$team_primary_record['team_name'];
 							echo "<br>";
 							echo "#".$team_primary_record['hashtag'];
+							echo "<img src='images/".$team_primary_record['image']."' alt='".$team_primary_record['team_name']." Logo' width=100, height=124>";
 						?>
 						</div>
 						<div class="team-info-column">
@@ -628,12 +641,13 @@
 					<div id="raspberry-racers" class="team-info">
 					<div class="team-info-column">
 						<?php
-							$raspberry_racers_query = "SELECT team_code, team_name, hashtag FROM teams WHERE team_id = 3";
+							$raspberry_racers_query = "SELECT team_code, team_name, hashtag, image FROM teams WHERE team_id = 3";
 							$raspberry_racers_result = mysqli_query($conn, $raspberry_racers_query);
 							$raspberry_racers_record = mysqli_fetch_assoc($raspberry_racers_result);
 							echo $raspberry_racers_record['team_code']." - ".$raspberry_racers_record['team_name'];
 							echo "<br>";
 							echo "#".$raspberry_racers_record['hashtag'];
+							echo "<img src='images/".$raspberry_racers_record['image']."' alt='".$raspberry_racers_record['team_name']." Logo' width=100, height=124>";
 						?>
 						</div>
 						<div class="team-info-column">
@@ -667,12 +681,13 @@
 					<div id="savage-speeders" class="team-info">
 					<div class="team-info-column">
 						<?php
-							$savage_speeders_query = "SELECT team_code, team_name, hashtag FROM teams WHERE team_id = 11";
+							$savage_speeders_query = "SELECT team_code, team_name, hashtag, image FROM teams WHERE team_id = 11";
 							$savage_speeders_result = mysqli_query($conn, $savage_speeders_query);
 							$savage_speeders_record = mysqli_fetch_assoc($savage_speeders_result);
 							echo $savage_speeders_record['team_code']." - ".$savage_speeders_record['team_name'];
 							echo "<br>";
 							echo "#".$savage_speeders_record['hashtag'];
+							echo "<img src='images/".$savage_speeders_record['image']."' alt='".$savage_speeders_record['team_name']." Logo' width=100, height=124>";
 						?>
 						</div>
 						<div class="team-info-column">
@@ -706,12 +721,13 @@
 					<div id="shining-swarm" class="team-info">
 					<div class="team-info-column">
 						<?php
-							$shining_swarm_query = "SELECT team_code, team_name, hashtag FROM teams WHERE team_id = 9";
+							$shining_swarm_query = "SELECT team_code, team_name, hashtag, image FROM teams WHERE team_id = 9";
 							$shining_swarm_result = mysqli_query($conn, $shining_swarm_query);
 							$shining_swarm_record = mysqli_fetch_assoc($shining_swarm_result);
 							echo $shining_swarm_record['team_code']." - ".$shining_swarm_record['team_name'];
 							echo "<br>";
 							echo "#".$shining_swarm_record['hashtag'];
+							echo "<img src='images/".$shining_swarm_record['image']."' alt='".$shining_swarm_record['team_name']." Logo' width=100, height=124>";
 						?>
 						</div>
 						<div class="team-info-column">
