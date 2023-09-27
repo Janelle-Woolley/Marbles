55cfd4a4c60a029cb7b3ba8e416cc7ef5b030d86
Janelle-Woolley <103225532+Janelle-Woolley@users.noreply.github.com>
2023-09-27 22:18:17 +1300

    Sprint 5 - added detail styling

--- update_user.php
+++ update_user.php
@@ -33,7 +33,7 @@
 			<div class="grid-item search_bar">
 				<form method="post" action="search.php">
                 	<input type="text" name="search">
-                	<input type="submit" name="submit" value="Search" class="search_button">
+                	<input type="submit" name="submit" value="Search" class="admin_button">
          		</form>
 			</div>
 			
@@ -80,7 +80,7 @@
 					</nav>
 					</div>
 		</div>
-
+		<div class="content">
     <!-- opens php -->
     <?php
 		$update_user = "UPDATE `users` SET `rank`='".$_POST['rank']."' WHERE `user_id`='".$_POST['user_id']."'";
@@ -95,10 +95,11 @@
 		}
 		
 	?>
+		</div>
 	<div class="footer_grid">
 			<!-- footer class from style sheet -->
 			<div class="footer">
-				footer
+				&copy; Jelle's Marble Race (Janelle Woolley)
 			</div>
 		</div>
 	</body>
