f5f64c419714cec92e0c9fa8dfa1537ffc963792
Janelle-Woolley <103225532+Janelle-Woolley@users.noreply.github.com>
2023-09-28 18:32:19 +1300

    Sprint 5 - comments and validating

--- insert_user.php
+++ insert_user.php
@@ -1,4 +1,4 @@
-<DOCTYPE html>
+<!DOCTYPE html>
 
 <!-- Sets document language to english -->
 <html lang="en">
@@ -13,7 +13,7 @@
     </head>
 
     <body>
-			<!-- opens php -->
+		<!-- opens php -->
     	<?php
 			session_start();
 			include '../marbles_mysqli.php';
@@ -21,92 +21,104 @@
 		?>
 		<!-- creates grid -->
 		<div class="grid-container">
-			<!-- logo class from style sheet -->
+			<!-- logo -->
 			<div class="grid-item logo">
-				 <img src="images/logo.png" alt="Jelle's Marble Run's Logo" width="200" height="105">
+				<img src="images/logo.png" alt="Jelle's Marble Run's Logo" width="200" height="105">
 			</div>
 			
-			<!-- banner class from style sheet -->
+			<!-- banner -->
 			<div class="grid-item banner">
 				Jelle's Marble Race
 			</div>
 			
-			<!-- search_bar class from style sheet -->
+			<!-- search bar -->
 			<div class="grid-item search_bar">
 				<form method="post" action="search.php">
                 	<input type="text" name="search">
                 	<input type="submit" name="submit" value="Search" class="admin_button">
          		</form>
 			</div>
-			
-			<!-- nav_bar class from style sheet -->
-					<div class="grid-item login_bar">
-					<nav>
+
+			<!-- nav bar login section -->
+			<div class="grid-item login_bar">
+				<nav>
 					<?php
-					// debug code
-					if((!isset($_SESSION['logged_in'])) or $_SESSION['logged_in'] != 1){
-						echo "<a href='login.php'> LOGIN </a>";
-					}
-					else {
-						echo "Logged In: ".$_SESSION['username'];
-						echo "<a href='process_logout.php'> LOGOUT </a>";
-					}
+						// displays login button if logged out 
+						if((!isset($_SESSION['logged_in'])) or $_SESSION['logged_in'] != 1){
+							echo "<a href='login.php'> LOGIN </a>";
+						}
+						// displays username and logout button if logged in
+						else {
+							echo "Logged In: ".$_SESSION['username'];
+							echo "<a href='process_logout.php'> LOGOUT </a>";
+						}
 					?>
-					</nav>
-					</div>
-					<div class="grid-item nav_bar">
-					<nav>
+				</nav>
+			</div>
+			
+			<!-- nav bar nav section -->
+			<div class="grid-item nav_bar">
+				<nav>
 					<!-- Creates links to each page with names -->
 					<a href="home.php"> HOME </a>
 					<a href="teams.php"> TEAMS </a>
 					<a href="events.php"> EVENT </a>
-					</nav>
-					</div>
-					<div class="grid-item admin_bar">
-					<nav>
-						<?php
+				</nav>
+			</div>
+	
+			<!-- nav bar admin button section -->
+			<div class="grid-item admin_bar">
+				<nav>
+					<?php
 						if(isset($_SESSION['logged_in'])){
 						$username = $_SESSION['username'];
+						// query to get user rank
 						$user_rank_query = "SELECT * FROM users WHERE username = '$username'";
 						$user_rank_result = mysqli_query($conn, $user_rank_query);
 						$user_rank_record = mysqli_fetch_assoc($user_rank_result);
 
+						// displays the admin button if the user is an admin or owner
 						if($user_rank_record['rank'] == "admin" || $user_rank_record['rank'] == "owner"){
 							echo "<a href='admin.php'> ADMIN </a>";
 						}
+						// displays the owner button if the user is an owner
 						if($user_rank_record['rank'] == "owner"){
 							echo "<a href='owner.php'> OWNER </a>";
 						}
 						}
-						?>
-					</nav>
-					</div>
+					?>
+				</nav>
+			</div>
 		</div>
+		
+		<!-- content -->
 		<div class="content">
-    <!-- opens php -->
-    <?php
-	
-	$username_insert = $_POST['username'];
-	$password = $_POST['password'];
-	$bcrypt_password = password_hash($password, PASSWORD_BCRYPT);
-		
-	$insert_user = "INSERT INTO users (username, password) VALUES ('$username_insert', '$bcrypt_password')";
-		
-	if(!mysqli_query($conn, $insert_user)){
-		echo 'Account Not Added';
-	}
-	
-	else{
-		echo 'Account Registered';
-	}
-	
-	header("refresh:2; url = login.php");
-	?>
+			<!-- opens php -->
+			<?php
+				$username_insert = $_POST['username'];
+				$password = $_POST['password'];
+				$bcrypt_password = password_hash($password, PASSWORD_BCRYPT);
+
+				// query to insert the user info into the database submited from the login page
+				$insert_user = "INSERT INTO users (username, password) VALUES ('$username_insert', '$bcrypt_password')";
+
+				// if the query doesn't work print
+				if(!mysqli_query($conn, $insert_user)){
+					echo 'Account Not Added';
+				// if the query does work
+				} else {
+					echo 'Account Registered';
+				}
+
+				header("refresh:2; url = login.php");
+			?>
 		</div>
-	<div class="footer_grid">
-			<!-- footer class from style sheet -->
+
+		<!-- footer, creates grid -->
+		<div class="footer_grid">
 			<div class="footer">
 				&copy; Jelle's Marble Race (Janelle Woolley)
 			</div>
 		</div>
 	</body>
+</html>
