028f1f664bc5df672207adae179fe2ee89235a86
Janelle-Woolley <103225532+Janelle-Woolley@users.noreply.github.com>
2023-09-28 18:00:39 +1300

    Sprint 5 - comments and validating

--- process_login.php
+++ process_login.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+
 <!-- Sets document language to english -->
 <html lang="en">
 
@@ -11,28 +13,32 @@
     </head>
 
     <body>
-  	<?php
-		session_start();
-		
-		include '../marbles_mysqli.php';
-		
-		$user = trim($_POST['username']);
-		$pass = trim($_POST['password']);
-		
-		$login_query = "SELECT password FROM users WHERE username = '". $user."'";
-		$login_result = mysqli_query($conn, $login_query);
-		$login_record = mysqli_fetch_assoc($login_result);
-		
-		$hash = $login_record['password'];
-		
-		$verify = password_verify($pass, $hash);
-		if($verify){
-			$_SESSION['logged_in'] = 1;
-			$_SESSION['username'] = $user;
-			header("Location: home.php");
-		}
-		else{
-			header("Location: login.php");
-		}
-	?>
+		<!-- opens php -->
+		<?php
+			session_start();
+			include '../marbles_mysqli.php';
+
+			$user = trim($_POST['username']);
+			$pass = trim($_POST['password']);
+
+			// query to select the password from the database based on the username input
+			$login_query = "SELECT password FROM users WHERE username = '". $user."'";
+			$login_result = mysqli_query($conn, $login_query);
+			$login_record = mysqli_fetch_assoc($login_result);
+
+			$hash = $login_record['password'];
+
+			// checks the password from the database is the same as the database input
+			$verify = password_verify($pass, $hash);
+			if($verify){
+				// logs the user in
+				$_SESSION['logged_in'] = 1;
+				$_SESSION['username'] = $user;
+				header("Location: home.php");
+			}
+			else{
+				header("Location: login.php");
+			}
+		?>
 	</body>
+</html>
