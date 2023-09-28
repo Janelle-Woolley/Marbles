2e4bbd6a61572eef12aa8373730cde55093ec9a8
Janelle-Woolley <103225532+Janelle-Woolley@users.noreply.github.com>
2023-09-28 16:55:14 +1300

    Sprint 5 - comments and validating

--- update_marble.php
+++ update_marble.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+
 <!-- Sets document language to english -->
 <html lang="en">
 
@@ -11,25 +13,24 @@
     </head>
 
     <body>
-	<!-- opens php -->
+		<!-- opens php -->
     	<?php
 			session_start();
 			include '../marbles_mysqli.php';
-
 		?>
-		<!-- creates grid -->
+    	<!-- creates grid -->
 		<div class="grid-container">
-			<!-- logo class from style sheet -->
+			<!-- logo -->
 			<div class="grid-item logo">
 				 <img src="images/logo.png" alt="Jelle's Marble Run's Logo" width="200" height="105">
 			</div>
 			
-			<!-- banner class from style sheet -->
+			<!-- banner -->
 			<div class="grid-item banner">
 				Jelle's Marble Race
 			</div>
 			
-			<!-- search_bar class from style sheet -->
+			<!-- search bar -->
 			<div class="grid-item search_bar">
 				<form method="post" action="search.php">
                 	<input type="text" name="search">
@@ -37,77 +38,94 @@
          		</form>
 			</div>
 			
-			<!-- nav_bar class from style sheet -->
-					<div class="grid-item login_bar">
-					<nav>
+			<!-- nav bar login section -->
+			<div class="grid-item login_bar">
+				<nav>
 					<?php
-					// debug code
-					if((!isset($_SESSION['logged_in'])) or $_SESSION['logged_in'] != 1){
-						echo "<a href='login.php'> LOGIN </a>";
-					}
-					else {
-						echo "Logged In: ".$_SESSION['username'];
-						echo "<a href='process_logout.php'> LOGOUT </a>";
-					}
+						// displays login button if logged out 
+						if((!isset($_SESSION['logged_in'])) or $_SESSION['logged_in'] != 1){
+							echo "<a href='login.php'> LOGIN </a>";
+						}
+						// displays username and logout button if logged in
+						else {
+							echo "Logged In: ".$_SESSION['username'];
+							echo "<a href='process_logout.php'> LOGOUT </a>";
+						}
 					?>
-					</nav>
-					</div>
-					<div class="grid-item nav_bar">
-					<nav>
+				</nav>
+			</div>
+			<!-- nav bar nav section -->
+			<div class="grid-item nav_bar">
+				<nav>
 					<!-- Creates links to each page with names -->
 					<a href="home.php"> HOME </a>
 					<a href="teams.php"> TEAMS </a>
 					<a href="events.php"> EVENT </a>
-					</nav>
-					</div>
-					<div class="grid-item admin_bar">
-					<nav>
-						<?php
+				</nav>
+			</div>
+			<!-- nav bar admin button section -->
+			<div class="grid-item admin_bar">
+				<nav>
+					<?php
 						if(isset($_SESSION['logged_in'])){
 						$username = $_SESSION['username'];
+						// query to get user rank
 						$user_rank_query = "SELECT * FROM users WHERE username = '$username'";
 						$user_rank_result = mysqli_query($conn, $user_rank_query);
 						$user_rank_record = mysqli_fetch_assoc($user_rank_result);
 
+						// displays the admin button if the user is an admin or owner
 						if($user_rank_record['rank'] == "admin" || $user_rank_record['rank'] == "owner"){
 							echo "<a href='admin.php'> ADMIN </a>";
 						}
+						// displays the owner button if the user is an owner
 						if($user_rank_record['rank'] == "owner"){
 							echo "<a href='owner.php'> OWNER </a>";
 						}
 						}
-						?>
-					</nav>
-					</div>
+					?>
+				</nav>
+			</div>
 		</div>
+		
+		<!-- content -->
 		<div class="content">
-    <!-- opens php -->
-    <?php
-		$update_competitors = "UPDATE competitors SET competitor_name='$_POST[marble_name]', team_id='$_POST[marble_team]' WHERE competitor_id='$_POST[competitor_id]'";
-		if(!mysqli_query($conn, $update_competitors))
-		{
-			echo 'Not Updated';
-			header("refresh:2; url = admin.php");
-		}
-		
-		$update_competitors_roles = "UPDATE competitors_roles SET roles_id='$_POST[marble_role]' WHERE competitor_id='$_POST[competitor_id]' AND roles_id='$_POST[current_role]'";
-		if(!mysqli_query($conn, $update_competitors_roles))
-		{
-			echo 'Not Updated';
-			// reset the competitor back
-			$reset_competitors = "UPDATE competitors SET competitor_name='$_POST[current_name]', team_id='$_POST[current_team]' WHERE competitor_id='$_POST[competitor_id]'";
-			header("refresh:2; url = admin.php");
-		} else {
-			echo 'Updated';
-			header("refresh:2; url = admin.php");
-		}
-		
-	?>
+    		<!-- opens php -->
+    		<?php
+				// query to update the marble info to the values submited from the admin page
+				$update_competitors = "UPDATE competitors SET competitor_name='$_POST[marble_name]', team_id='$_POST[marble_team]' WHERE competitor_id='$_POST[competitor_id]'";
+				
+				// if the query doesn't work print not updated
+				if(!mysqli_query($conn, $update_competitors))
+				{
+					echo 'Not Updated';
+					header("refresh:2; url = admin.php");
+				}
+
+				// query to update the marble role info to the values submited from the admin page
+				$update_competitors_roles = "UPDATE competitors_roles SET roles_id='$_POST[marble_role]' 
+											WHERE competitor_id='$_POST[competitor_id]' 
+											AND roles_id='$_POST[current_role]'";
+				
+				// if the query doesn't work print not updated
+				if(!mysqli_query($conn, $update_competitors_roles))
+				{
+					echo 'Not Updated';
+					// reset the competitor back
+					$reset_competitors = "UPDATE competitors SET competitor_name='$_POST[current_name]', team_id='$_POST[current_team]' WHERE competitor_id='$_POST[competitor_id]'";
+					header("refresh:2; url = admin.php");
+				// if the query does work print updated
+				} else {
+					echo 'Updated';
+					header("refresh:2; url = admin.php");
+				}
+			?>
 		</div>
-	<div class="footer_grid">
-			<!-- footer class from style sheet -->
+		<!-- footer, creates grid -->
+		<div class="footer_grid">
 			<div class="footer">
 				&copy; Jelle's Marble Race (Janelle Woolley)
 			</div>
 		</div>
 	</body>
+</html>
