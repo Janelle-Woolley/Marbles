8ce49230a685ea67c9e7e8346d5986da711d717a
Janelle-Woolley <103225532+Janelle-Woolley@users.noreply.github.com>
2023-09-28 21:23:48 +1300

    Sprint 5 - commenting and validating

--- insert_marble.php
+++ insert_marble.php
@@ -13,111 +13,126 @@
     </head>
 
     <body>
-			<!-- opens php -->
+		<!-- opens php -->
     	<?php
 			session_start();
 			include '../marbles_mysqli.php';
-
 		?>
+
 		<!-- creates grid -->
 		<div class="grid-container">
-			<!-- logo class from style sheet -->
+			<!-- logo -->
 			<div class="grid-item logo">
-				 <img src="images/logo.png" alt="Jelle's Marble Run's Logo" width="200" height="105">
+				<img src="images/logo.png" alt="Jelle's Marble Run's Logo" width="200" height="105">
 			</div>
 			
-			<!-- banner class from style sheet -->
+			<!-- banner -->
 			<div class="grid-item banner">
 				Jelle's Marble Race
 			</div>
 			
-			<!-- search_bar class from style sheet -->
+			<!-- search bar -->
 			<div class="grid-item search_bar">
 				<form method="post" action="search.php">
                 	<input type="text" name="search">
                 	<input type="submit" name="submit" value="Search" class="admin_button">
          		</form>
 			</div>
-			
-			<!-- nav_bar class from style sheet -->
-					<div class="grid-item login_bar">
-					<nav>
+
+			<!-- nav bar login section -->
+			<div class="grid-item login_bar">
+				<nav>
 					<?php
-					// debug code
-					if((!isset($_SESSION['logged_in'])) or $_SESSION['logged_in'] != 1){
-						echo "<a href='login.php'> LOGIN </a>";
-					}
-					else {
-						echo "Logged In: ".$_SESSION['username'];
-						echo "<a href='process_logout.php'> LOGOUT </a>";
-					}
+						// displays login button if logged out 
+						if((!isset($_SESSION['logged_in'])) or $_SESSION['logged_in'] != 1){
+							echo "<a href='login.php'> LOGIN </a>";
+						}
+						// displays username and logout button if logged in
+						else {
+							echo "Logged In: ".$_SESSION['username'];
+							echo "<a href='process_logout.php'> LOGOUT </a>";
+						}
 					?>
-					</nav>
-					</div>
-					<div class="grid-item nav_bar">
-					<nav>
+				</nav>
+			</div>
+			
+			<!-- nav bar nav section -->
+			<div class="grid-item nav_bar">
+				<nav>
 					<!-- Creates links to each page with names -->
 					<a href="home.php"> HOME </a>
 					<a href="teams.php"> TEAMS </a>
 					<a href="events.php"> EVENT </a>
-					</nav>
-					</div>
-					<div class="grid-item admin_bar">
-					<nav>
-						<?php
+				</nav>
+			</div>
+	
+			<!-- nav bar admin button section -->
+			<div class="grid-item admin_bar">
+				<nav>
+					<?php
 						if(isset($_SESSION['logged_in'])){
 						$username = $_SESSION['username'];
+						// query to get user rank
 						$user_rank_query = "SELECT * FROM users WHERE username = '$username'";
 						$user_rank_result = mysqli_query($conn, $user_rank_query);
 						$user_rank_record = mysqli_fetch_assoc($user_rank_result);
 
+						// displays the admin button if the user is an admin or owner
 						if($user_rank_record['rank'] == "admin" || $user_rank_record['rank'] == "owner"){
 							echo "<a href='admin.php'> ADMIN </a>";
 						}
+						// displays the owner button if the user is an owner
 						if($user_rank_record['rank'] == "owner"){
 							echo "<a href='owner.php'> OWNER </a>";
 						}
 						}
-						?>
-					</nav>
-					</div>
+					?>
+				</nav>
+			</div>
 		</div>
+		
+		<!-- content -->
 		<div class="content">
-	<!-- opens php -->
-	<?php
-		
-		$competitor_name = $_POST['marble_name'];
-		$team_id = $_POST['marble_team'];
-		$role_id = $_POST['marble_role'];
-		
-		$insert_competitors = "INSERT INTO competitors (competitor_name, team_id) VALUES ('$competitor_name', '$team_id')";
-		
-		if(!mysqli_query($conn, $insert_competitors)){
-			echo 'Unable to add Marble';
-			header("refresh:2; url = admin.php");
-		}
-		
-		$competitor_id_query = "SELECT competitor_id FROM competitors WHERE competitor_name = '$competitor_name'";
-		$competitor_id_result = mysqli_query($conn, $competitor_id_query);
-    	$competitor_id_record = mysqli_fetch_assoc($competitor_id_result);
-		$competitor_id = $competitor_id_record['competitor_id'];
-		
-		$insert_competitors_roles = "INSERT INTO competitors_roles (competitor_id, roles_id) VALUES ('$competitor_id', '$role_id')";
-		
-		if(!mysqli_query($conn, $insert_competitors_roles)){
-			echo 'Unable to add Marble';
-			// TO ADD: delete competitor because we don't want a competitor without a role
-		} else {
-			echo 'Marble Added';
-			header("refresh:2; url = admin.php");
-		}		
-		
-	?>
+			<!-- opens php -->
+			<?php
+				$competitor_name = $_POST['marble_name'];
+				$team_id = $_POST['marble_team'];
+				$role_id = $_POST['marble_role'];
+
+				// query to insert the marble info into the database submited from the admin page
+				$insert_competitors = "INSERT INTO competitors (competitor_name, team_id) VALUES ('$competitor_name', '$team_id')";
+
+				// if the query doesn't work
+				if(!mysqli_query($conn, $insert_competitors)){
+					echo 'Unable to add Marble';
+					header("refresh:2; url = admin.php");
+				}
+
+				// query to select the competitor_id of the marble just added to the database
+				$competitor_id_query = "SELECT competitor_id FROM competitors WHERE competitor_name = '$competitor_name'";
+				$competitor_id_result = mysqli_query($conn, $competitor_id_query);
+				$competitor_id_record = mysqli_fetch_assoc($competitor_id_result);
+				$competitor_id = $competitor_id_record['competitor_id'];
+
+				// query to insert the marble role info into the database submited from the admin page
+				$insert_competitors_roles = "INSERT INTO competitors_roles (competitor_id, roles_id) VALUES ('$competitor_id', '$role_id')";
+
+				// if the query doesn't work
+				if(mysqli_query($conn, $insert_competitors_roles)){
+					echo 'Unable to add Marble role';
+					header("refresh:2; url = admin.php");
+				// if the query does work
+				} else {
+					echo 'Marble Added';
+					header("refresh:2; url = admin.php");
+				}				
+			?>
 		</div>
-	<div class="footer_grid">
-			<!-- footer class from style sheet -->
+		<!-- footer, creates grid -->
+		<div class="footer_grid">
 			<div class="footer">
 				&copy; Jelle's Marble Race (Janelle Woolley)
 			</div>
 		</div>
 	</body>
+</html>
