f264af701ddf8bbf2f89d9e2c38f31d278267f02
Janelle-Woolley <103225532+Janelle-Woolley@users.noreply.github.com>
2023-09-29 23:32:12 +1300

    Sprint 5 - Final Commit

--- delete_event.php
+++ delete_event.php
@@ -7,7 +7,7 @@
     Sets charset to utf-8 (setting character encoding)
     Links to CSS style sheet-->
     <head>
-        <title> DELETE EVENT - Jelle's Marble Leauge </title>
+        <title> DELETE EVENT - Jelle's Marble Runs </title>
         <meta charset="utf-8">
 		<link rel="stylesheet" href="style.css">
     </head>
@@ -27,7 +27,7 @@
 			
 			<!-- banner -->
 			<div class="grid-item banner">
-				Jelle's Marble Race
+				Jelle's Marble Runs
 			</div>
 			
 			<!-- search bar -->
@@ -110,7 +110,7 @@
 		<!-- footer, creates grid -->
 		<div class="footer_grid">
 			<div class="footer">
-				&copy; Jelle's Marble Race (Janelle Woolley)
+				&copy; Jelle's Marble Runs (Janelle Woolley)
 			</div>
 		</div>
 	</body>
